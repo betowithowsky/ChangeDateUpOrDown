c02558e2f984b4c6a44f90fdfde83b16b8f53c9b
= <betowithowsky@gmail.com>
2019-02-06 17:13:22 -0200

    adicionando projeto

---
+++ TesteVagaFrontEnd.Test/DateExtensionsTest.cs
@@ -0,0 +1,199 @@
+﻿using NUnit.Framework;
+using TesteVagaFrontEnd.Models;
+
+namespace TesteVagaFrontEnd.Test
+{
+    [TestFixture]
+    public class DateExtensionsTest
+    {
+        [Test]
+        public void ChangeDate_AddOneMinute_ReturnDateWithOneMinuteMore()
+        {
+            var extension = new DateExtensions();
+
+            var result = extension.ChangeDate("01/03/2010 23:00", '+', 1);
+
+            Assert.AreEqual("01/03/2010 23:01", result);
+        }
+
+        [Test]
+        public void ChangeDate_AddTenMinute_ReturnDateWithTenMinuteMore()
+        {
+            var extension = new DateExtensions();
+
+            var result = extension.ChangeDate("01/03/2010 23:00", '+', 10);
+
+            Assert.AreEqual("01/03/2010 23:10", result);
+        }
+
+        [Test]
+        public void ChangeDate_AddOneHour_ReturnDateWithOneHourMore()
+        {
+            var extension = new DateExtensions();
+
+            var result = extension.ChangeDate("01/03/2010 22:00", '+', 60);
+
+            Assert.AreEqual("01/03/2010 23:00", result);
+        }
+
+        [Test]
+        public void ChangeDate_AddOneHour_ReturnDateWithOneDayMore()
+        {
+            var extension = new DateExtensions();
+
+            var result = extension.ChangeDate("01/03/2010 23:00", '+', 60);
+
+            Assert.AreEqual("02/03/2010 00:00", result);
+        }
+
+        [Test]
+        public void ChangeDate_AddTwoHour_ReturnDateWithOneDayMore()
+        {
+            var extension = new DateExtensions();
+
+            var result = extension.ChangeDate("01/03/2010 23:00", '+', 120);
+
+            Assert.AreEqual("02/03/2010 01:00", result);
+        }
+
+        [Test]
+        public void ChangeDate_AddOneDay_ReturnDateWithOneDayMore()
+        {
+            var extension = new DateExtensions();
+
+            var result = extension.ChangeDate("01/03/2010 23:00", '+', 1440);
+
+            Assert.AreEqual("02/03/2010 23:00", result);
+        }
+
+        [Test]
+        public void ChangeDate_AddTreeDay_ReturnDateWithTreeDayMore()
+        {
+            var extension = new DateExtensions();
+
+            var result = extension.ChangeDate("01/03/2010 23:00", '+', 4320);
+
+            Assert.AreEqual("04/03/2010 23:00", result);
+        }
+
+        [Test]
+        public void ChangeDate_AddOneMouth_ReturnDateWithOneMouthMore()
+        {
+            var extension = new DateExtensions();
+
+            var result = extension.ChangeDate("01/03/2010 23:00", '+', 44640);
+
+            Assert.AreEqual("01/04/2010 23:00", result);
+        }
+
+        [Test]
+        public void ChangeDate_AddFirstDayOfFebuaryAndAddThirtyDays_ReturnDateThirtyDayOfMarch()
+        {
+            var extension = new DateExtensions();
+
+            var result = extension.ChangeDate("01/02/2010 23:00", '+', 43200);
+
+            Assert.AreEqual("03/03/2010 23:00", result);
+        }
+
+        [Test]
+        public void ChangeDate_AddOneYear_ReturnDateToNextYear()
+        {
+            var extension = new DateExtensions();
+
+            var result = extension.ChangeDate("01/02/2010 23:00", '+', 525600);
+
+            Assert.AreEqual("01/02/2011 23:00", result);
+        }
+
+        [Test]
+        public void ChangeDate_Challenge_ReturnDateToChallenge()
+        {
+            var extension = new DateExtensions();
+
+            var result = extension.ChangeDate("01/03/2010 23:00", '+', 4000);
+
+            Assert.AreEqual("04/03/2010 17:40", result);
+        }
+
+        [Test]
+        public void ChangeDate_TakingAnHour_ReturnDateOneHourLess()
+        {
+            var extension = new DateExtensions();
+
+            var result = extension.ChangeDate("01/03/2010 23:00", '-', 60);
+
+            Assert.AreEqual("01/03/2010 22:00", result);
+        }
+
+        [Test]
+        public void ChangeDate_TakingTwoHour_ReturnDateTwoHourLess()
+        {
+            var extension = new DateExtensions();
+
+            var result = extension.ChangeDate("01/03/2010 23:00", '-', 120);
+
+            Assert.AreEqual("01/03/2010 21:00", result);
+        }
+
+        [Test]
+        public void ChangeDate_TakingOneDay_ReturnDateOneDayLess()
+        {
+            var extension = new DateExtensions();
+
+            var result = extension.ChangeDate("01/03/2010 23:00", '-', 1440);
+
+            Assert.AreEqual("28/02/2010 23:00", result);
+        }
+
+        [Test]
+        public void ChangeDate_TakingThreeDays_ReturnDateThreeDaysLess()
+        {
+            var extension = new DateExtensions();
+
+            var result = extension.ChangeDate("01/03/2010 23:00", '-', 4320);
+
+            Assert.AreEqual("26/02/2010 23:00", result);
+        }
+
+        [Test]
+        public void ChangeDate_TakingOneMonth_ReturnDateOneMonthLess()
+        {
+            var extension = new DateExtensions();
+
+            var result = extension.ChangeDate("01/03/2010 23:00", '-', 43200);
+
+            Assert.AreEqual("30/01/2010 23:00", result);
+        }
+
+        [Test]
+        public void ChangeDate_TakingOneYear_ReturnDateOneYearLess()
+        {
+            var extension = new DateExtensions();
+
+            var result = extension.ChangeDate("01/03/2010 23:00", '-', 525600);
+
+            Assert.AreEqual("01/03/2009 23:00", result);
+        }
+
+        [Test]
+        public void ChangeDate_TakingTwoYear_ReturnDateTwoYearLess()
+        {
+            var extension = new DateExtensions();
+
+            var result = extension.ChangeDate("01/03/2010 23:00", '-', 1051200);
+
+            Assert.AreEqual("01/03/2008 23:00", result);
+        }
+
+        [Test]
+        public void ChangeDate_TestNegativeValue_ReturnDateTwoYearLessWithNegativeValue()
+        {
+            var extension = new DateExtensions();
+
+            var result = extension.ChangeDate("01/03/2010 23:00", '-', +1051200);
+
+            Assert.AreEqual("01/03/2008 23:00", result);
+        }
+    }
+}
---
+++ TesteVagaFrontEnd/Models/DateExtensions.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TesteVagaFrontEnd.Models
+{
+    public class DateExtensions
+    {
+        private Dictionary<int, int> months = new Dictionary<int, int>()
+        {
+            { 1, 31 },
+            { 2, 28 },
+            { 3, 31 },
+            { 4, 30 },
+            { 5, 31 },
+            { 6, 30 },
+            { 7, 31 },
+            { 8, 31 },
+            { 9, 30 },
+            { 10, 31 },
+            { 11, 30 },
+            { 12, 31 }
+        };
+
+        public string ChangeDate(string date, char op, long value)
+        {
+            string[] piecesTextDate = date.Split('/', ' ', ':');
+
+            int minutos = Convert.ToInt32(piecesTextDate[4]);
+            int horas = Convert.ToInt32(piecesTextDate[3]);
+            int ano = Convert.ToInt32(piecesTextDate[2]);
+            int mes = Convert.ToInt32(piecesTextDate[1]);
+            int dias = Convert.ToInt32(piecesTextDate[0]);
+
+            long convertValue = Math.Abs(value);
+
+            if(op != '+' || op != '-')
+            {
+                Console.WriteLine("Erro! insira um operador valido. + ou - ");
+            }
+
+            
+
+            if(op == '+')
+            {
+                while (convertValue > 0)
+                {
+                    convertValue--;
+                    minutos++;
+
+                    if (minutos >= 60)
+                    {
+                        minutos = 00;
+                        horas++;
+                    }
+                    if (horas > 23)
+                    {
+                        horas = 00;
+                        dias++;
+                    }
+                    if (dias > months[mes])
+                    {
+                        dias = 01;
+                        mes++;
+                    }
+                    if (mes > 12)
+                    {
+                        mes = 01;
+                        ano++;
+                    }
+                }
+            }
+
+            if (op == '-')
+            {
+                while (convertValue > 0)
+                {
+                    convertValue--;
+                    minutos--;
+
+                    if (minutos < 00)
+                    {
+                        minutos = 59;
+                        horas--;
+                    }
+                    if (horas < 00)
+                    {
+                        horas = 23;
+                        dias--;
+                    }
+                    if (dias < 01)
+                    {
+                        if (mes == 1)
+                        {
+                            dias = months[12];
+                            mes--;
+                        }
+                        else {
+                            dias = months[mes - 1];
+                            mes--;
+                        }
+                    }
+                    if (mes < 01)
+                    {
+                        mes = 12;
+                        ano--;
+                    }
+                }
+            }
+            
+            return dias.ToString("D2") + "/" + mes.ToString("D2") + "/" + ano.ToString("D2") + " " + horas.ToString("D2") + ":" + minutos.ToString("D2");
+        }
+    }
+}
